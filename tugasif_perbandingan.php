e59d1ff075b779bda433eefac7ee773a6d6b030b
yanto-sugiyanto <71365660+yanto-sugiyanto@users.noreply.github.com>
2020-10-08 12:15:41 +0700

    Add files via upload

---
+++ tugasif_perbandingan.php
@@ -0,0 +1,18 @@
+<?php
+
+$a=100;
+$b=20;
+
+if($a > $b){
+  echo $a, " Lebih Besar dari ",$b;
+}else if ($a < $b):
+  echo $a, " Lebih Kecil dari ",$b;
+else:
+  echo $a, " Sama dengan ",$b;
+
+
+endif
+
+
+?>
+
